8dd7cc4a35350fdf0b99b0664a836f66d494cda5
Alexey Porfirev <porfirev.alexey@gmail.com>
2022-03-12 15:48:56 +0400

    add comments

--- src/main/java/threads/ArrayRecursiveTask.java
+++ src/main/java/threads/ArrayRecursiveTask.java
@@ -8,6 +8,12 @@ public class ArrayRecursiveTask extends RecursiveTask<Integer> {
     private int end;
     private int[] array;
 
+    /**
+     * Создание рекурсивной задачи
+     * @param start начальный индекс в массиве
+     * @param end конечный индекс в массиве
+     * @param array массив с целыми числами
+     */
     public ArrayRecursiveTask(int start, int end, int[] array) {
         this.array = array;
         this.start = start;
@@ -21,6 +27,8 @@ public class ArrayRecursiveTask extends RecursiveTask<Integer> {
             case 0: return 0;
             case 1: return array[start];
             case 2: return array[start] + array[start + 1];
+//            case 3: return array[start] + array[start + 1] + array[start + 2];
+//            case 4: return array[start] + array[start + 1] + array[start + 2] + array[start + 3] ;
             default: return forkTasksAndGetResult();
         }
     }
@@ -28,11 +36,8 @@ public class ArrayRecursiveTask extends RecursiveTask<Integer> {
     private int forkTasksAndGetResult() {
         final int middle = (end - start) / 2 + start;
         ArrayRecursiveTask task1 = new ArrayRecursiveTask(start, middle, array);
-
         ArrayRecursiveTask task2 = new ArrayRecursiveTask(middle, end, array);
-
         invokeAll(task1, task2);
-
         return task1.join() + task2.join();
 
     }
--- src/main/java/tools/ArrayGenerator.java
+++ src/main/java/tools/ArrayGenerator.java
@@ -4,6 +4,12 @@ import java.util.Random;
 
 public class ArrayGenerator {
 
+    /**
+     * Метод формирования массива целых чисел
+     * @param size заднный размер массива
+     * @param maxValue заданное максимлаьное значение для элементов массива
+     * @return ссылка на создынный массив
+     */
     public static int[] generateArray(int size, int maxValue) {
         int[] array = new int[size];
         Random rand = new Random(System.currentTimeMillis());
@@ -13,6 +19,11 @@ public class ArrayGenerator {
         return array;
     }
 
+    /**
+     * Вычисление суммы элементов массива
+     * @param array массив с целыми числами
+     * @return посчитанное значение суммы элементов массива
+     */
     public static int getSum(int[] array) {
         int sum = 0;
         for(int el: array) {
@@ -21,6 +32,11 @@ public class ArrayGenerator {
         return sum;
     }
 
+
+    /**
+     * Вывод содержимого массива на печать
+     * @param array массив с целыми числами
+     */
     public static void printArray(int[] array) {
         for(int el: array) {
             System.out.println(el);
